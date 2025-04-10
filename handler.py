50562ef7d4c8abadd53a809834172ee98363311d
Illia Ivashchenko <illia.ivashchenko1@gmail.com>
2025-04-10 02:45:55 -0400

    Change the call to the FRED API

--- handler.py
+++ handler.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Environment variables
-FRED_API_KEY = os.getenv("FRED_API")
+FRED_API_KEY = os.getenv("FRED_API_KEY")
 
 FRED_INDICATORS = {
     "CPI": "CPIAUCSL",
